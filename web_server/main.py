604e948685cefda9fec313b6a414238f25623c81
Jean Ruggiero <jeanruggiero@gmail.com>
2019-05-26 14:40:48 -0700

    Added party mode!

--- web_server/main.py
+++ web_server/main.py
@@ -8,39 +8,95 @@ response_404 = """HTTP/1.0 404 NOT FOUND
 <h1>404 Not Found</h1>
 """
 
+response_500 = """HTTP/1.1 500 Internal Server KeyError
+
+<h1>500 Internal Server Error<h1>
+"""
+
 response_template = """HTTP/1.0 200 OK
 
-%s
+{:s}
 """
 import machine
-import ntptime, utime
-from machine import RTC
-seconds = ntptime.time()
-rtc = RTC()
-rtc.datetime(utime.localtime(seconds))
+from time import sleep
+from machine import RTC, Pin
+
+
+wled = Pin(13)
+wled = machine.PWM(wled)
+bled = Pin(2)
+bled = machine.PWM(bled)
+gled = Pin(5)
+gled = machine.PWM(gled)
+yled = Pin(12)
+yled = machine.PWM(yled)
+rled = Pin(4)
+rled = machine.PWM(rled)
+switch = Pin(10)
 
 adc = machine.ADC(0)
 
-def time():
+def light_on():
+    wled.value(1)
+
     body = """<html>
- <body>
- <h1>Time</h1>
- <p>%s</p>
- </body>
- </html>
-    """ % str(rtc.datetime())
+    <body>
+    <p>The LED is on!</p>
+    </body>
+    </html>
+    """
 
-    return response_template % body
+    return response_template.format(body)
 
+def lights_off():
+    for led in [wled, bled, yled, gled, rled]:
+        led.duty(0)
+
+    body = """<html>
+    <body>
+    <p>The LED is off!</p>
+    </body>
+    </html>
+    """
+
+    return response_template.format(body)
+
+def switch_status():
+    body = "{state: " + str(switch.value()) + "}"
+    return response_template.format(body)
+
+def light():
+    body = "{value: " + str(adc.read()) + "}"
+    return response_template.format(body)
+
+def party_mode():
+    body = "Let's get the party started!"
+    leds = [wled, bled, yled, gled, rled]
+    while True:
+        for led in leds:
+            led.duty(int(1.00679**adc.read()))
+            sleep(0.05)
+            print(int(1.00679**adc.read()))
+
+        for led in leds:
+            led.duty(0)
+            sleep(0.05)
+
+    return response_template.format(body)
 
 def dummy():
     body = "This is a dummy endpoint"
 
-    return response_template % body
+    return response_template.format(body)
 
 handlers = {
     'time': time,
     'dummy': dummy,
+    'light_on': light_on,
+    'lights_off': lights_off,
+    'switch_status': switch_status,
+    'light': light,
+    'party_mode': party_mode,
 }
 
 def main():
@@ -71,9 +127,9 @@ def main():
             handler = handlers[path.strip('/').split('/')[0]]
             response = handler()
         except KeyError:
-            response = 404_response
+            response = response_404
         except Exception as e:
-            response = ""
+            response = response_500
             print(str(e))
 
         # A handler returns an entire response in the form of a multi-line string.
